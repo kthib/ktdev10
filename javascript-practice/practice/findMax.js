876d14cd6d9cdbc1a1f64a0b9e6687c6eae19bf0
Krystal Thibault <krystal.thibault@icloud.com>
2020-02-19 07:41:25 -0600

    javascript fun!

---
+++ javascript-practice/practice/findMax.js
@@ -0,0 +1,13 @@
+var array = [3 , 6, 2, 56, 32, 5, 89, 32];
+var findMax= 0;
+
+for (i=0; i<=findMax;i++){
+    if (array[i]>findMax) {
+        var findMax=array[i];
+    }
+}
+
+
+
+
+console.log(findMax);
